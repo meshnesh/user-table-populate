cdb90740f1be99a08bac14b737cfc97b1ffae8eb
meshnesh <tonnie.nesh@gmail.com>
2016-10-19 14:43:24 +0300

    save button working

--- js/app.js
+++ js/app.js
@@ -7,10 +7,22 @@ function createTable() {
     for (i=0; i<rows; i +=1 ) {
         tblHtml += '<tr>';
         for (j=0; j<cols; j +=1 ) {
-         tblHtml += '<td> <INPUT TYPE="TEXT" NAME="name" SIZE="20"></td>';
+         tblHtml += '<td> <INPUT TYPE="TEXT" NAME="name" SIZE="20" placeholder="content"></td>';
         }
           tblHtml += '</tr>';
     }
-    tblHtml += '<hr>'+'<h3>'+'Please add data respectively to the table'+'</h3>'+'<h4>'+'<span>'+'NB:'+'</span>'+'The first row is the content header'+'</h4>'+'</table>'+'</form>';
+    tblHtml += '<hr>'+'<h3>'+'Please add data respectively to the table'
+    +'</h3>'+'<h4>'+'<span>'+'NB:'+'</span>'
+    +'The first row is the content header'+'</h4>'+'</table>'+'</form>'+'<a id="savefile" class="success button" href="#">Save File</a>';
     document.getElementById('tb_container').innerHTML = tblHtml;
+   
+
+	$('#savefile').click(function () {
+	alert("file saved");
+});
 }
+
+
+
+
+
