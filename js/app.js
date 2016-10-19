88e5356f06f8c96d8294d3c28acf8f6b8454d1f1
meshnesh <tonnie.nesh@gmail.com>
2016-10-19 16:14:11 +0300

    completed the site

--- js/app.js
+++ js/app.js
@@ -7,20 +7,33 @@ function createTable() {
     for (i=0; i<rows; i +=1 ) {
         tblHtml += '<tr>';
         for (j=0; j<cols; j +=1 ) {
-         tblHtml += '<td> <INPUT TYPE="TEXT" NAME="name" SIZE="20" placeholder="content"></td>';
+         tblHtml += '<td> <input id="content" type="number" placeholder="content" name="value"></td>';
         }
           tblHtml += '</tr>';
     }
     tblHtml += '<hr>'+'<h3>'+'Please add data respectively to the table'
     +'</h3>'+'<h4>'+'<span>'+'NB:'+'</span>'
-    +'The first row is the content header'+'</h4>'+'</table>'+'</form>'+'<a id="savefile" class="success button" href="#">Save File</a>';
+    +'The first row is the content header'+'</h4>'+'</table>'+'<a id="savefile" class="success button" href="#">Save File</a>'+'</form>';
     document.getElementById('tb_container').innerHTML = tblHtml;
    
 
-	$('#savefile').click(function () {
-	alert("file saved");
+	$('#savefile').on('click',function(){
+console.log("file saved");
+  
+    // if ( document.getElementById("content").content === " ") {
+    // 	console.log('Cannot be empty');
+    // }else {
+       
+    //    console.log("file saved");
+    // }
 });
+	
+		
+
+	
 }
+
+
 
 
 
