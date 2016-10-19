28f652e6eb24e351d3acf8d4201f1fd5a22ccba9
meshnesh <tonnie.nesh@gmail.com>
2016-10-19 12:27:19 +0300

    added the javascript table code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-// ********************************************************
-// NAV BAR
-// 
-$(function(){
-
-    // Returns width of browser viewport
-    var browser = $(window).width();
-    // Returns width of HTML document
-    var document = $(document).width();
-
-    $('.nav .toggle-nav').click(function(e){
-        $('.nav .nav-mobile').addClass('style-mobile').slideToggle('slow');
-        e.preventDefault();
-    });
-
-});
-
+function createTable() {
+ document.getElementById('tb_container').innerHTML = "" ;
+    var rows  = document.getElementById('tb_rows').value;
+     var cols  = document.getElementById('tb_cols').value;
+ 
+    tblHtml = '<form method="post">'+'<table>'
+    for (i=0; i<rows; i +=1 ) {
+        tblHtml += '<tr>';
+        for (j=0; j<cols; j +=1 ) {
+         tblHtml += '<td> <INPUT TYPE="TEXT" NAME="name" SIZE="20"></td>';
+        }
+          tblHtml += '</tr>';
+    }
+    tblHtml += '</table>'+'</form>';
+    document.getElementById('tb_container').innerHTML = tblHtml;
+}
